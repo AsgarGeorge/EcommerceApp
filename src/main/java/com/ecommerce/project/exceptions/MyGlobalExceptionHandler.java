6ac447e6d661fef812ed4c54e32fb9ecd048dc13
Asgar_George <asgarantony@gmail.com>
2024-07-16 18:10:56 +0530

    adding custom exception

---
+++ src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.ecommerce.project.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class MyGlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> myMethodArgumentValidException(MethodArgumentNotValidException e){
+        Map<String,String> response = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(
+                err -> {
+                    String fieldName = ((FieldError)err).getField();
+                    String message = err.getDefaultMessage();
+                    response.put(fieldName,message);
+                }
+
+        );
+        System.out.println();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(ResourceNotFound.class)
+    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFound e){
+        String message = e.getMessage();
+        return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
+
+    }
+}
---
+++ src/main/java/com/ecommerce/project/exceptions/ResourceNotFound.java
@@ -0,0 +1,22 @@
+package com.ecommerce.project.exceptions;
+
+public class ResourceNotFound extends RuntimeException {
+    String resourceName;
+    String field;
+    String fieldName;
+    Long fieldId;
+
+    public ResourceNotFound(String resourceName, String field, String fieldName) {
+        super(String.format("%s not found with %s: %s",resourceName,field,fieldName));
+        this.resourceName = resourceName;
+        this.field = field;
+        this.fieldName = fieldName;
+    }
+
+    public ResourceNotFound(Long fieldId, String field, String resourceName) {
+        super(String.format("%s not found with %s: %d",resourceName,field,fieldId));
+        this.fieldId = fieldId;
+        this.field = field;
+        this.resourceName = resourceName;
+    }
+}
